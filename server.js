3427a49aa2c9fef4f39147604b6bd4ae469217bb
bernardfernando <jagathf@gmail.com>
2023-07-31 23:21:38 +0100

    amends

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bp = require("body-parser");
 
 const app = express();
 app.use(cors());
+app.use(bp.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
-
-  response.send('test request received')
-
-})
+app.get("/test", (request, response) => {
+  response.send("test request received");
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
